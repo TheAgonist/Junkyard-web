f3180a4e49cab9333c72ed9f3a28cafe7355ea9b
Stefan Georgiev <death.trapbg@gmail.com>
2016-02-28 20:04:37 +0200

    imgs+newevent/problem

---
+++ NewPorE.php
@@ -0,0 +1,16 @@
+<head>
+<title>Page Title</title>
+<link rel="stylesheet" type="text/css" href="css/Home.css">
+</head>
+
+<body>
+	<div align="center" class="logo">
+		<img class="img img3" src="images/Logo2.png" width="124" height="124">
+	</div>
+	<div align="center">
+
+		<button class="button button1" onclick="parent.location='eventForm.php'">Create Events</button>
+		<button class="button button1"
+			onclick="parent.location='problemForm.php'">Create Problem</button>
+	</div>
+</body>
